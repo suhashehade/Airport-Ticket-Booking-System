882ccc2546f43a033b8f451bfff9c93da403e6ed
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-06-23 11:02:34 -0700

    chore: setup ticket and flight models

---
+++ src/Models/Flight.cs
@@ -0,0 +1,50 @@
+class Flight
+{
+    public enum FlightClass
+    {
+        Economy,
+        Business,
+        FirstClass
+    }
+
+    private double _price;
+    private FlightClass _class;
+    public FlightClass Class
+    {
+        get { return _class; }
+        set
+        {
+            _class = value;
+
+            switch (_class)
+            {
+                case FlightClass.Economy: _price = 100; break;
+                case FlightClass.Business: _price = 200; break;
+                case FlightClass.FirstClass: _price = 300; break;
+            }
+        }
+    }
+    public double Price
+    {
+        get { return _price; }
+    }
+
+    public string? DepartureCountry { get; set; }
+    public string? DestinationCountry { get; set; }
+    public string? DepartureDate { get; set; }
+    public string? DepartureAirport { get; set; }
+    public string? ArrivalAirport { get; set; }
+
+    public Flight() { }
+
+    public Flight(string departureCountry, string destinationCountry, string departureDate,
+                  string departureAirport, string arrivalAirport, FlightClass flightClass)
+    {
+        DepartureCountry = departureCountry;
+        DestinationCountry = destinationCountry;
+        DepartureDate = departureDate;
+        DepartureAirport = departureAirport;
+        ArrivalAirport = arrivalAirport;
+        Class = flightClass;
+    }
+}
---
+++ src/Models/Ticket.cs
@@ -0,0 +1,36 @@
+using PassengerSpace;
+
+class Ticket
+{
+    private string? _ticketId;
+    private Passenger? _passenger;
+    private Flight? _flight;
+
+    public string? TicketId
+    {
+        get { return _ticketId; }
+        set { _ticketId = value; }
+    }
+
+    public Passenger? Passenger
+    {
+        get { return _passenger; }
+        set { _passenger = value; }
+    }
+
+    public Flight? Flight
+    {
+        get { return _flight; }
+        set { _flight = value; }
+    }
+
+    public Ticket() { }
+
+
+    public Ticket(Passenger passenger, Flight flight)
+    {
+        TicketId = Guid.NewGuid().ToString();
+        Passenger = passenger;
+        Flight = flight;
+    }
+}
